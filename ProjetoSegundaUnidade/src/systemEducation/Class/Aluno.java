41087ec3ee2131e7702323a9e2f456150c6f51ac
iLevsPHP_ <202215210015@ifba.edu.br>
2023-08-21 22:19:14 -0300

    Adição na Turma e nos alunos

--- ProjetoSegundaUnidade/src/systemEducation/Class/Aluno.java
+++ ProjetoSegundaUnidade/src/systemEducation/Class/Aluno.java
@@ -4,12 +4,14 @@ public class Aluno extends Pessoa {
 	private String periodoRef;
 	private String matricula;
 	private String curso;
+	private Disciplina disciplina;
 
-	public Aluno(String periodoRef, String matricula, String curso, int id, String nome, String cpf, int dia, int mes,
-			int ano) {
+	public Aluno(String periodoRef, String matricula, String curso, Disciplina disciplina, int id, String nome, String cpf, int dia, int mes, int ano) {
 		super(id, nome, cpf, dia, mes, ano);
 		this.periodoRef = periodoRef;
 		this.matricula = matricula;
 		this.curso = curso;
+		this.disciplina = disciplina;
 	}
+	
 }
--- ProjetoSegundaUnidade/src/systemEducation/Class/Turma.java
+++ ProjetoSegundaUnidade/src/systemEducation/Class/Turma.java
@@ -1,5 +1,17 @@
 package systemEducation.Class;
 
 public class Turma {
-
+	private String turno;
+	private String nivelEscolar;
+	private String anoLetivo;
+	private String frequencia;
+	private Aluno[] aluno;
+	
+	public Turma(String turno,String nivelEscolar,String anoLetivo, String frequencia, int numAlunos) {
+		this.turno = turno;
+		this.nivelEscolar = nivelEscolar;
+		this.anoLetivo = anoLetivo;
+		this.frequencia = frequencia;
+		
+	}
 }
