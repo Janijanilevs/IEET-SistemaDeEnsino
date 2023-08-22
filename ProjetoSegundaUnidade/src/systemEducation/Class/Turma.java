5b1101ca1f89ab74021effff36be57ca66dd44af
iLevsPHP_ <202215210015@ifba.edu.br>
2023-08-21 22:46:03 -0300

    Atualizando Turmas.java

--- ProjetoSegundaUnidade/src/systemEducation/Class/Turma.java
+++ ProjetoSegundaUnidade/src/systemEducation/Class/Turma.java
@@ -5,13 +5,60 @@ public class Turma {
 	private String nivelEscolar;
 	private String anoLetivo;
 	private String frequencia;
-	private Aluno[] aluno;
+	private Aluno[] alunos;
 	
 	public Turma(String turno,String nivelEscolar,String anoLetivo, String frequencia, int numAlunos) {
 		this.turno = turno;
 		this.nivelEscolar = nivelEscolar;
 		this.anoLetivo = anoLetivo;
 		this.frequencia = frequencia;
-		
+		this.alunos = new Aluno[numAlunos];
 	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	public String getNivelEscolar() {
+		return nivelEscolar;
+	}
+
+	public void setNivelEscolar(String nivelEscolar) {
+		this.nivelEscolar = nivelEscolar;
+	}
+
+	public String getAnoLetivo() {
+		return anoLetivo;
+	}
+
+	public void setAnoLetivo(String anoLetivo) {
+		this.anoLetivo = anoLetivo;
+	}
+
+	public String getFrequencia() {
+		return frequencia;
+	}
+
+	public void setFrequencia(String frequencia) {
+		this.frequencia = frequencia;
+	}
+
+	public Aluno[] getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(Aluno alunos, int numAluno) {
+		this.alunos[numAluno] = alunos;
+	}
+	
+	public void addAlunos(Aluno aluno) {
+		static int num = 0;
+		this.alunos[num] = aluno;
+	}
+	
+	
 }
