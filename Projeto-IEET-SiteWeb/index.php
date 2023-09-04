33f568d01859e9484767cf6e67a3dfb95a518cda
iLevsPHP_ <202215210015@ifba.edu.br>
2023-09-03 22:23:46 -0300

    Criando Pastas

--- Projeto-IEET-SiteWeb/index.php
+++ Projeto-IEET-SiteWeb/index.php
@@ -6,6 +6,7 @@
     <link href="estilos/estiloPrincipal.css" rel="stylesheet"><!-- Isso são links de arquivos diferentes para coisas mais inportantes como o menu e o rodapé onde todos vão herdar essas estilizaçõees-->
     <link href="estilos/estiloIndex.css" rel="stylesheet"> <!-- E o arquivo css da propria pagina para estilizar a mesma-->
     <title>IEET- Sistema de Ensino</title>
+    <!-- Essa será a pagina de login e nos utilizaremos essa pagina apenas para enviar o usuario as paginas e facilitar a mobilidade-->
 </head>
 <body>
     <header>
