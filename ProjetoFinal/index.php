cbe3043bb8b91d49fe0d8e4f7775c851bc43d326
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-12-03 14:13:23 -0300

    Conclusão e organiznado inicial e topo principal

--- ProjetoFinal/index.php
+++ ProjetoFinal/index.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 use IeetSite\Core\Router;
 
-require __DIR__ . "/app/View/componentes/topo.view.php"; #Teste de rodapé e topo
-require __DIR__ . "/app/View/componentes/rodape.view.php"; #Teste de rodapé e topo
+require __DIR__ . "/app/View/componentes/topo.view.php"; #Teste de  topo
+
+require __DIR__ . "/app/View/inicial.view.php"; # Teste de inicial
+
+require __DIR__ . "/app/View/componentes/rodape.view.php"; #Teste de rodapé 
+
 die;
 
 require __DIR__ . "/vendor/autoload.php";
