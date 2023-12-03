6464f66ffae29b06565e02824a8ee25ae755f5c4
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-12-03 14:10:11 -0300

    Página inicial quase totalemente cooncluida

--- ProjetoFinal/app/View/componentes/topo.view.php
+++ ProjetoFinal/app/View/componentes/topo.view.php
@@ -6,12 +6,8 @@
     <link rel="stylesheet" type="text/css" href="public/css/estilo.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Water+Brush&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Timmana&family=Water+Brush&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Timmana&family=Urbanist&family=Water+Brush&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Timmana&family=Urbanist&family=Water+Brush&family=Zilla+Slab&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Overpass+Mono&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Timmana&family=Ubuntu&family=Urbanist&family=Water+Brush&family=Zilla+Slab&display=swap" rel="stylesheet">
     <title><?=$titulo?></title>
 </head>
 <body>
@@ -54,6 +50,7 @@
         <div id="div-possibilidades">
             <section class="section-padrao"> 
                 <div class="section-usuario">
+                    <img src="./public/imagens/AlunoInicial.png">
                     <h2>Verifique seu boletim;</h2>
                     <h2>Complete? Conclua suas atividades;</h2>
                     <h2>Conheça seu progresso;</h2>
@@ -63,6 +60,7 @@
             </section>
             <section class="section-padrao">
                 <div class="section-usuario"> 
+                    <img src="./public/imagens/DiretoriaInicial.png">
                     <h2>Notifique rapidamente;</h2>
                     <h2>Gerencie suas turmas;</h2>
                     <h2>Informações organizadas;</h2>
@@ -72,6 +70,7 @@
             </section>
             <section class="section-padrao">
                 <div class="section-usuario">
+                    <img src="./public/imagens/ProfessorIncial.png">
                     <h2>Organize suas turmas;</h2>
                     <h2>Lecione atividades;</h2>
                     <h2>Forneça materiais de estudo;</h2>
@@ -79,5 +78,11 @@
                 </div>
                 <a href="login">Acesse como Professor</a>
             </section>
+        </div>
+        <div class="video-inicial">
+            <h1>+IEET Pra Você:</h1>        
+        </div>
+        <div class="video-inicial">
+            <iframe width="100%" height="500" src="https://www.youtube.com/embed/s4nXf0Ltvic?si=lFSjO1E17ujOAnhI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     </main>
