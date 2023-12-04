43b151e3e96df3fcf310b80666a5cc7cc3b1ce28
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-12-04 20:42:34 -0300

    comtt

--- ProjetoFinal/app/View/DirecaoMain.view.php
+++ ProjetoFinal/app/View/DirecaoMain.view.php
@@ -1,10 +1,11 @@
 <?=componente('topo/topoDirecao')?>
     <main id="MainRegistroEscolar">
-        <div id="Titulo"><h1>Registro Escolar</h1></div>
+        <div id="TituloDirecaoMain"><h1>Registro Escolar</h1></div>
 
         <div id="EscolhaUsuario">
             <div id="Aluno"><img src="./public/imagens/AlunoDirecao.png"></div>
             <div id="Professor"><img src="./public/imagens/ProfessorDirecao.png"></div>
         </div>
+       
     </main>
 <?=componente('rodape')?>
