e9903b4716496d9c1ed2b084ee07ade7816c22e2
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-12-04 13:37:01 -0300

    inicioDirecao

--- ProjetoFinal/app/View/DirecaoMain.view.php
+++ ProjetoFinal/app/View/DirecaoMain.view.php
@@ -1,3 +1,10 @@
 <?=componente('topo/topoDirecao')?>
-<main id="MainRegistrarEscola">
+    <main id="MainRegistroEscolar">
+        <div id="TituloRegistrarEscola"><h1>Registro Escolar</h1></div>
+
+        <div id="EscolhaUsuario">
+            <div id="Aluno"><img src="./public/imagens/AlunoDirecao.png"></div>
+            <div id="Professor"><img src="./public/imagens/ProfessorDirecao.png"></div>
+        </div>
+    </main>
 <?=componente('rodape')?>
