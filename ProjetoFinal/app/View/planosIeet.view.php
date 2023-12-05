e4b5ec7f76718aa6e51242eb5076ee180157a427
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-12-05 10:49:48 -0300

    commit

--- ProjetoFinal/app/View/planosIeet.view.php
+++ ProjetoFinal/app/View/planosIeet.view.php
@@ -1,5 +1,37 @@
 <?php componente('topo')?>
     <main id="MainPlanos">
         <div id="TituloPlanos"> <h1>Nossos Planos</h1></div>
+        <div id="MostrarPlanos">
+            <div class="Planos">
+                <h4>Plano  IEET</h4>
+                <h4>400 usuaríos disponiveis.</h4>
+                <h4>Taxa de acrescimo de usuario de 1.5%</h4>
+                <h4>Preço:R$5.000</h4>
+            </div>
+            <div class="Planos">
+                <h4>Plano  IEET+</h4>
+                <h4>600 usuarios disponiveis.</h4>
+                <h4>Taxa de acrescento de usuario de 1.0%</h4>
+                <h4>Preço:R$7.000</h4>
+            </div>
+            <div class="Planos">
+                <h4>Plano  IEET++</h4>
+                <h4>800 usuarios disponiveis.</h4>
+                <h4>Taxa de acrescento de usuario de 0.8%</h4>
+                <h4>Preço:R$10.000</h4>
+            </div>
+            <div class="Planos">
+                <h4>Plano  IEET Plus</h4>
+                <h4>1000 usuarios disponiveis.</h4>
+                <h4>Taxa de acrescento de usuario de 0.5%</h4>
+                <h4>Preço:R$15.000</h4>
+            </div>
+        </div>
+        <div id="AdquirirPlanos">
+            <div class="BtnAdquirir"><h4>Adquirir Plano</h4></div>
+            <div class="BtnAdquirir"><h4>Adquirir Plano</h4></div>
+            <div class="BtnAdquirir"><h4>Adquirir Plano</h4></div>
+            <div class="BtnAdquirir"><h4>Adquirir Plano</h4></div>
+        </div>
     </main>
 <?php componente('rodape')?>
