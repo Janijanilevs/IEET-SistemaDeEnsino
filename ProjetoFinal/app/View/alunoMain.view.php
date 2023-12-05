ab2c46c8ac7b69dcc78dfe73168da22a1c82a171
iLevsPHP_ <202215210015@ifba.edu.br>
2023-12-04 21:25:45 -0300

    Continuando Traumas

--- ProjetoFinal/app/View/alunoMain.view.php
+++ ProjetoFinal/app/View/alunoMain.view.php
@@ -7,12 +7,12 @@
                 <img src="./public/imagens/perfil.png">
             </div>
             <div id="infAluno">
-                <div class="dadAluno"><h2>Nome: Aluno da Silva</h2></div>
-                <div class="dadAluno"><h2>Situação: Matriculado</h2></div>
-                <div class="dadAluno"><h2>Matricula: 2022202200</h2></div>
-                <div class="dadAluno"><h2>Data de Ingresso: 2018</h2></div>
-                <div class="dadAluno"><h2>CPF: 048-156-485.56</h2></div>
-                <div class="dadAluno"><h2>CRE 7,5</h2></div>
+                <div class="dados"><h2>Nome: Aluno da Silva</h2></div>
+                <div class="dados"><h2>Situação: Matriculado</h2></div>
+                <div class="dados"><h2>Matricula: 2022202200</h2></div>
+                <div class="dados"><h2>Data de Ingresso: 2018</h2></div>
+                <div class="dados"><h2>CPF: 048-156-485.56</h2></div>
+                <div class="dados"><h2>CRE 7,5</h2></div>
             </div>
             
         </div>
---
+++ ProjetoFinal/app/View/turmaAlunos.view.php
@@ -0,0 +1,21 @@
+<?=componente('topo/topoProfessor')?>
+    <main >
+        <div id="MainTurmaAlunos">
+            <h1 id="titTurma">2º Ano B</h1>
+            
+                
+            
+            
+            <img src="./public/imagens/perfil.png">
+            <div id="infAluno">
+                <div class="dados"><h2>Nome: Profesor da Silva</h2></div>
+                <div class="dados"><h2>Situação: Efetivo</h2></div>
+                <div class="dados"><h2>Matricula: 2022202200</h2></div>
+                <div class="dados"><h2>Data de Ingresso: 2018</h2></div>
+                <div class="dados"><h2>CPF: 048-156-485.56</h2></div>
+                <div class="dados"><h2>Formação: Engenharia/h2></div>
+            </div>
+            
+        </div>
+    </main>
+<?=componente('rodape')?>
