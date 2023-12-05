ea187ab677fae00c1979d7858d07ff65c350c11c
iLevsPHP_ <202215210015@ifba.edu.br>
2023-12-05 11:27:24 -0300

    direção main terminada

--- ProjetoFinal/app/View/DirecaoMain.view.php
+++ ProjetoFinal/app/View/DirecaoMain.view.php
@@ -2,9 +2,19 @@
     <main id="MainRegistroEscolar">
         <div id="TituloDirecaoMain"><h1>Registro Escolar</h1></div>
 
-        <div id="EscolhaUsuario">
-            <div id="Aluno"><img src="./public/imagens/AlunoDirecao.png"></div>
-            <div id="Professor"><img src="./public/imagens/ProfessorDirecao.png"></div>
+        <div id="EscolhaUsuarioDirecao">
+            <div class="adUsuario">
+                <img src="./public/imagens/AlunoDirecao.png">
+                <div><h2>Aluno</h2></div>
+            </div>
+            <div class="adUsuario">
+                <img src="./public/imagens/ProfessorDirecao.png">
+                <div><h2>Professor</h2></div>
+            </div>
+            <div class="adUsuario">
+                <img src="./public/imagens/turma2ano.png">
+                <div><h2>Turma</h2></div>
+            </div>
         </div>
        
     </main>
