8a55342527f7e3feb6f584fa199a190188df144f
iLevsPHP_ <202215210015@ifba.edu.br>
2023-12-07 14:38:43 -0300

    reg

---
+++ ProjetoFinal/app/View/escolaRegistros.view.php
@@ -0,0 +1,13 @@
+<?php componente('topo/topoAdmin')?>
+<main>
+    <div id="AdicionaDirecao">
+        <div id="TituloDirecaoMain"><h1>Registar Escola e Direção</h1></div>
+        <div id="AdUsuarioDirecao">
+            <div id="regDir" class="adUsuario">
+                <img src="./public/imagens/livrosRegAdmin.png">
+                <div><a href="<?=linkrota('adicionaDirecao')?>"><h2>Inserir Dados</h2></a></div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php componente('rodape')?>
