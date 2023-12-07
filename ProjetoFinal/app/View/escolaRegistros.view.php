834b1bdffd08ca45ee3a215f317b439f0b812499
iLevsPHP_ <202215210015@ifba.edu.br>
2023-12-07 16:34:27 -0300

    Registros Completos

--- ProjetoFinal/app/View/escolaRegistros.view.php
+++ ProjetoFinal/app/View/escolaRegistros.view.php
@@ -6,18 +6,33 @@
             <div id="InfTabReg">
                 <div class="nEscola"></div>
                 <div class="nCnpj"><h2>CNPJ</h2></div>
-                <div class="nDir"><h2>DIREÇÃO</h2></div>
+                <div class="nCont"><h2>CONTATO</h2></div>
                 <div class="nSit"><h2>SITUAÇÃO</h2></div>
                 <div class="nUsu"><h2>USUÁRIOS</h2></div>
             </div>
             <div class="linha1TabReg">
                 <div class="nEscola"><h2>Ifba Campus Brumado</h2></div>
                 <div class="nCnpj"><h2>123456</h2></div>
-                <div class="nDir"><h2>Celton Ribeiro</h2></div>
+                <div class="nCont"><h2>celtinha@gmail.com</h2></div>
+                <div class="nSit"><h2>Pendente</h2></div>
+                <div class="nUsu"><h2>1020  </h2></div>
+            </div>
+            <div class="linha2TabReg">
+                <div class="nEscola"><h2>Ifba Campus Brumado</h2></div>
+                <div class="nCnpj"><h2>123456</h2></div>
+                <div class="nCont"><h2>celtinha@gmail.com</h2></div>
+                <div class="nSit"><h2>Pendente</h2></div>
+                <div class="nUsu"><h2>1020  </h2></div>
+            </div>
+            <div class="linha1TabReg">
+                <div class="nEscola"><h2>Ifba Campus Brumado</h2></div>
+                <div class="nCnpj"><h2>123456</h2></div>
+                <div class="nCont"><h2>celtinha@gmail.com</h2></div>
                 <div class="nSit"><h2>Pendente</h2></div>
                 <div class="nUsu"><h2>1020  </h2></div>
             </div>
         </div>
+        <div id="altSit"><a href="<?=linkrota('situacaoPag')?>"><h1>Alterar Situação</h1></a></div>
     </div>
 </main>
 <?php componente('rodape')?>
