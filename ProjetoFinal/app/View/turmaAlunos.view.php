52df7900b386696e7987b31450fe87571737d313
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2023-12-08 00:10:34 -0300

    BackEnd das páginas de inserir notas(back de notas não funciona ainda), turmas alunos, turmas

--- ProjetoFinal/app/View/turmaAlunos.view.php
+++ ProjetoFinal/app/View/turmaAlunos.view.php
@@ -1,28 +1,45 @@
-<?=componente('topo/topoProfessor')?>
+<?php
+
+use IeetSite\Model\DAO\AlunoDAO;
+use IeetSite\Model\DAO\TurmaDAO;
+#var_dump($_GET['nomeDisciplina']);
+#var_dump(TurmaDAO::getById($_GET['idTurma']));
+$contador = 0;
+foreach(AlunoDAO::getAll() as $count => $aluno) if($aluno->__get('Turma_idTurma') == $_GET['idTurma']) $contador = $count;
+componente('topo/topoProfessor')?>
     <main >
         <div id="MainTurmaAlunos">
-            <div id="titTurma"><h1>2º Ano B</h1></div>
+            <div id="titTurma"><h1><?=TurmaDAO::getById($_GET["idTurma"])->__get('nome')?></h1></div>
             <div id="extTopo">
-                <div class="exTurma"><h2>Quantidade de Alunos</h2><h3>19</h3></div>
-                <div class="exTurma"><h2>Carga Horaria</h2><h3>180 Horas</h3></div>
-                <div class="exTurma"><h2>Materia</h2><h3>Matematica</h3></div>
+                <div class="exTurma"><h2>Quantidade de Alunos</h2><h3><?= $contador ?></h3></div>
+                <div class="exTurma"><h2>Carga Horaria</h2><h3><?= $_GET['cargaHoraria']?> Horas </h3></div>
+                <div class="exTurma"><h2>Materia</h2><h3><?= $_GET['nomeDisciplina'] ?></h3></div>
             </div>
             
             <div id="listAProf">
+
+                <?php foreach(AlunoDAO::getAll() as $count => $aluno){ 
+                    if($aluno->__get('Turma_idTurma') == $_GET['idTurma']){?>
+
                 <div class="AlunoList">
                     <img src="./public/imagens/perfil.png">
-                    <h2>Cleiton da Silva</h2>
+                    <h2><?= $aluno->__get('nome')?></h2>
                     <h2>N</h2>                 
                     <h2>N</h2>
                     <h2>N</h2>
                     <h2>M</h2>
-                    <div id="modNotas"><a href="<?=linkrota('inserirNotas')?>"><h2>Modificar Notas</h2></a></div>
+                    <div id="modNotas"><a href="<?=linkrota('inserirNotas')?>?idAluno=<?=$aluno->__get('id')?>"><h2>Modificar Notas</h2></a></div>
                 </div>
+                
+                <?php }}?>
+
             </div>
+
             <div id="butonAdVe">
                 <div class="btsAV"><a href="<?=linkrota('inserirAtividades')?>"><h2>Adicionar Atividades ou Material</h2></a></div>
                 <div class="btsAV"><a href="<?=linkrota('verificaAtividade')?>"><h2>Verificar Atividades</h2></a></div>
             </div>
+
         </div>
     </main>
 <?=componente('rodape')?>
--- ProjetoFinal/app/View/turmas.view.php
+++ ProjetoFinal/app/View/turmas.view.php
@@ -2,8 +2,10 @@
 
 use IeetSite\Model\DAO\DisciplinaDAO;
 use IeetSite\Model\DAO\TurmaDAO;
+
+componente('topo/topoProfessor');
+
 ?>
-<?=componente('topo/topoProfessor')?>
     <main id="mainTurmas">
         <h1>Minhas Turmas - Disciplinas</h1>
         <div id="MainProfessorTurmas">  
@@ -13,12 +15,12 @@ use IeetSite\Model\DAO\TurmaDAO;
                     if($_SESSION['__usuario'] == $disciplina->__get('usuario_professor_id')){?>
                         
                     
-
                     <div id="caixaTMProf" class="CaixaTarefasMateriais">
                         <h3 class="TituloTarefa"><?= TurmaDAO::getById($disciplina->__get('Turma_idTurma'))->__get('nome'); ?> </h3>
                         <div class="caixaTarefa">
                             <h3>Disciplina: <?= $disciplina->__get('nome') ?> </h3>
-                            <a href="<?=linkrota('turmaAlunos')?>"><h3>Ver Turma</h3></a>
+                            <a href="<?= linkrota('turmaAlunos')?>?idTurma= <?= $disciplina->__get('Turma_idTurma')?>&nomeDisciplina= <?= $disciplina->__get('nome')?>&cargaHoraria= <?= $disciplina->__get('carga_Horaria')?> ">
+                            <h3>Ver Turma</h3></a>
                         </div>
                     </div>
 
